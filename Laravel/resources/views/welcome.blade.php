6b9021134bcdd36f98e4da8da0c0c7abe6ec1047
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-01-17 23:54:11 +0600

    17.01.2023

--- Laravel/resources/views/welcome.blade.php
+++ Laravel/resources/views/welcome.blade.php
@@ -1,5 +1,72 @@
+<?php
+// Задача 1. Напишите функцию которая проверяет, что строка начинается на 'http://' . Если это так – верните true, если не так – false.
+echo "<h2>Задача 1</h2>";
+$url_http = "http://";
+$url_https = "https://";
+
+function checkUrl(string $url){
+    if (str_starts_with($url, "http://")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+print("url_http = ".checkUrl($url_http)."<br>"); // Выводит 1
+print("url_https = ".checkUrl($url_https)); // Выводит ничего
+
+
+
+
+
+// Задача 2. Напишите функцию, которая определяет пол по ФИО.
+echo "<br><br><br><br><br>";
+echo "<h2>Задача 2</h2>";
+$data_names = [
+    'mans' => ['Мансур', 'Кирилл', 'Егор'],
+    'womans' => ['Настя', 'Катя', 'Герда']
+];
+$data_input = ['Герда', 'Кирилл', 'Мансур'];
+
+function manOrWoman(array $verification, array $reconciliation) { // проверка, сверка
+    foreach ($verification as $key => $value) {
+        if (in_array($value, $reconciliation['mans'], false)) {
+            echo "$value: мужик";
+        }
+        if (in_array($value, $reconciliation['womans'], false)) {
+            echo "$value: девушка";
+        }
+        echo "<br>";
+    }
+}
+manOrWoman($data_input, $data_names);
+
+
+// Задача 3. Напишите функцию getDivisors, которая параметром принимает число и возвращает массив его делителей (чисел, на которое делится данное число).
+echo "<br><br><br><br><br>";
+echo "<h2>Задача 3</h2>";
+
+function getDivisors(int $number):array {
+    $massDell = [];
+    $number_old = $number;
+    do {
+        if ($number_old%$number==0) {
+            $massDell[] = $number;
+        }
+        $number -= 1;
+    } while ($number > 1);
+    $massDell[] = 1;
+    echo "Число $number_old<br>";
+    return $massDell;
+}
+
+echo '<pre>';
+
+print_r(getDivisors(10));
+echo '</pre>';
+
+?>
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,92 +75,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+        
     </body>
 </html>
