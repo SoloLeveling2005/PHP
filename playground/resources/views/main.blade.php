ecd7090e4b69fde151574bd862192508320465b0
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-03-01 21:49:21 +0600

    01.03.2023

--- playground/resources/views/main.blade.php
+++ playground/resources/views/main.blade.php
@@ -6,11 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 {{--    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">--}}
-    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <title>@yield('title')</title>
 </head>
 <body>
 @yield('main_content')
-<script src="./bootstrap/js/bootstrap.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
 </html>
